264605b96cba1fbf4e247e9f47734c888fd973d7
Peter Berkenbosch <peter@stembolt.com>
2016-10-08 11:09:16 -0400

    delegate currency on ledger entries to the store_credit add scope for chronological listing add scope for reverse chronological listing

--- core/app/models/spree/store_credit_ledger_entry.rb
+++ core/app/models/spree/store_credit_ledger_entry.rb
@@ -2,5 +2,10 @@
 module Spree
   class StoreCreditLedgerEntry < Spree::Base
     belongs_to :store_credit
+
+    scope :chronological, -> { order(:created_at) }
+    scope :reverse_chronological, -> { order(created_at: :desc) }
+
+    delegate :currency, to: :store_credit
   end
 end
