bd95a0e9ac6a26903d20f01a6740dba68a2e523a
Peter Berkenbosch <peter@stembolt.com>
2016-10-11 13:52:47 -0400

    specify a shorting index name on the ledger entry originator

--- core/db/migrate/20161006211441_create_spree_store_credit_ledger_entry.rb
+++ core/db/migrate/20161006211441_create_spree_store_credit_ledger_entry.rb
@@ -3,7 +3,8 @@ class CreateSpreeStoreCreditLedgerEntry < ActiveRecord::Migration[5.0]
     create_table :spree_store_credit_ledger_entries do |t|
       t.decimal :amount, precision: 8, scale: 2, default: 0.0, null: false
       t.references :store_credit, index: true
-      t.references :originator, polymorphic: true, index: false
+      index_name = "idx_spree_sc_ledger_entries_on_orig_type_and_orig_id"
+      t.references :originator, polymorphic: true, index: { name: index_name }
       t.timestamps null: false
     end
   end
